ea86fbeb309406cde04a23b03d24193dbb4c0c39
Adam Smith and Grant Rotzien <adam@>
2017-10-26 16:44:28 -0700

    attempt to add function to encrypt input message, work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 var alphaNumChar = /[a-zA-Z0-9]/;
+var squareSize;
+
+var encryptedMessage = function(unencryptedArray) {
+  var encryptedArray = [];
+  for (var i = 0; encryptedArray.length <= unencryptedArray.length; i++) {
+    for (index = 0; index < unencryptedArray.length; index++) {
+      var counter = index;
+      debugger;
+      encryptedArray.push(unencryptedArray[counter]);
+
+    };
+  };
+  console.log(encryptedArray);
+  return encryptedArray;
+};
 
 var cryptoMsg = function(input) {
   var removeCaps = input.toLowerCase();
@@ -8,11 +23,12 @@ var cryptoMsg = function(input) {
     if (alphaNumChar.test(letter)) {
       outputArray.push(letter);
     };
-  })
-  console.log(outputArray.length);
-  var squareSize = Math.ceil(Math.sqrt(outputArray.length));
-  console.log(squareSize);
-}
+  });
+  squareSize = Math.ceil(Math.sqrt(outputArray.length));
+
+  var finalMessage = encryptedMessage(outputArray);
+  console.log(finalMessage);
+};
 
 
 
